f2ee45ec237d8bea8c76357376bfd7a456e1420a
RoshanAnsy <roshanansy123@gmail.com>
2024-04-04 15:44:19 +0530

    double toast  message issue solved

--- server/config/cloudnary.js
+++ server/config/cloudnary.js
@@ -3,10 +3,11 @@ const cloudnary=require("cloudinary");
 
 exports.CloudnaryConnect=()=>{
     try{
-        cloudnary.config({
+        cloudnary.v2.config({
             cloud_name:process.env.CLOUD_NAME,
             api_key:process.env.API_KEY,
             api_secret:process.env.API_SECRET,
+            secure: true,
         });
     }
     catch(error){
